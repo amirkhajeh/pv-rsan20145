a8bd89a2632368e7d39778be0d9cffee05636969
amirkhajeh <amir.khajeh82@gmail.com>
2016-09-29 20:53:20 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '');
+define('286497323:AAEmSzyHdHPWkiXUH3AifKmicBk8nhGDsr8', '');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
